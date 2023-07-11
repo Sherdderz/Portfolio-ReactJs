66ef34d13a1e30b43dfe4fe355ab4f0cd8efdff2
Sherdderz <rizkygaez@gmail.com>
2023-07-11 12:04:54 +0700

    ganti foto

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,5 +1,4 @@
-import Proj3 from "../assets/proj3.png";
-import Proj4 from "../assets/proj4.png";
+import script from "../assets/script.png";
 import yarts1 from "../assets/yarts/yarts1.png";
 import helmify3 from "../assets/helmify/helmify3.png";
 export const ProjectList = [
@@ -17,27 +16,27 @@ export const ProjectList = [
     },
     {
         name: "Simple API CRUD Using Golang",
-        image: Proj3,
-        skills: "Golang",
-        detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
-    },
-    {
-        name: "Test Coding Kitalulus",
-        image: Proj4,
+        image: script,
         skills: "Golang",
         detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
     },
     {
         name: "Cheat Script for Android Game",
-        image: Proj4,
+        image: script,
         skills: "LUA",
         detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
     },
-    {
-        name: "Simple Credit Calculation App",
-        image: Proj4,
-        skills: "Java",
-        detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
-    },
+    // {
+    //     name: "",
+    //     image: Proj4,
+    //     skills: "-",
+    //     detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
+    // },
+    // {
+    //     name: "Simple Credit Calculation App",
+    //     image: Proj4,
+    //     skills: "Java",
+    //     detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
+    // },
 ];
 
