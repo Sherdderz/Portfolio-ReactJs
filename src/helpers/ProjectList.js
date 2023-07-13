d311776a00c1c660d5e57a9b913e8b9f3aa32c7e
Sherdderz <rizkygaez@gmail.com>
2023-07-13 14:41:16 +0700

    add details

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,42 +1,45 @@
 import script from "../assets/script.png";
 import api from "../assets/api.png";
+import game from "../assets/game.png";
+import credit from "../assets/credit.png";
 import yarts1 from "../assets/yarts/yarts1.png";
 import helmify3 from "../assets/helmify/helmify3.png";
+
 export const ProjectList = [
     {
         name: "Helmify E-Commerce Website",
         image: helmify3,
         skills: "HTML, CSS, JavaScript, PHP",
-        detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
+        detail: "Helmify is simple E-Commerce website that developed using HTML, CSS, JavaScript, and PHP",
     },
     {
         name: "Yarts Image Sharing Website",
         image: yarts1,
         skills: "HTML, CSS, JavaScript, PHP",
-        detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
+        detail: "Yarts is some simple website that can Create, Read, Update, and Delete images",
     },
     {
         name: "Simple API CRUD Using Golang",
         image: api,
         skills: "Golang",
-        detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
+        detail: "This Simple CRUD API project is made to test CRUD function with 3 variables name, price, and description using Postman", 
     },
     {
         name: "Cheat Script for Android Game",
         image: script,
         skills: "LUA",
-        detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
+        detail: "The function of this script is to upgrade basic stats such as HP, ATK, DEF, and CP on Naruto x Boruto Ninja Voltage android game.",
     },
     {
         name: "Simple Credit Calculation App",
-        image: api,
-        skills: "Java",
-        detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
+        image: credit,
+        skills: "Java & React Native",
+        detail: "Credit calculation app is made to calculate vehicle's credit per-month",
     },
     {
-        name: "-",
-        image: api,
-        skills: "-",
+        name: "Endless Runner Unity 3D",
+        image: game,
+        skills: "C# & Unity 3D",
         detail: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet con etiam. Lorem ipsum dolor sit amet con etiam",
     },
 ];
